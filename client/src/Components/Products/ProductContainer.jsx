ea6f92e93c009d290379b023949a571b29d04d71
Pablo Martín Escudero <escuderopablo@hotmail.com.ar>
2021-11-19 22:01:37 -0300

    Update

--- client/src/Components/Products/ProductContainer.jsx
+++ client/src/Components/Products/ProductContainer.jsx
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from 'react'
-import { useLogin } from '../../Contexts/LoginContext';
 import ProductForm from './ProductForm/ProductForm'
 import Products from './ProductList/Products'
 
 const ProductsContainer = () => {
     const[products, setProducts]=useState(null);
-    const login = useLogin();
     useEffect(()=>{
        fetch('/api/home')
        .then(res=>res.json())
